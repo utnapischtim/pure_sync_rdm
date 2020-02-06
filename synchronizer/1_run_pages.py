58e12a248a5ad6dfcbebafbb05dedd568f7aa3a6
mat-gro <m.grosso@tugraz.at>
2020-02-06 11:10:58 +0100

    Get updates and changes ok

--- synchronizer/1_run_pages.py
+++ synchronizer/1_run_pages.py
@@ -2,5 +2,5 @@ from PTR_1 import pureToInvenio
 
 inst_pti = pureToInvenio()
 
-inst_pti.get_pure_by_page(11, 25, 100)             # pag_begin, pag_end, pag_size
+inst_pti.get_pure_by_page(17, 18, 100)             # pag_begin, pag_end, pag_size
 
---
+++ synchronizer/4.2_get_pure_changes.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import os
+from datetime import date
+from pprint import pprint     
+
+dirpath = os.path.dirname(os.path.abspath(__file__))
+
+headers = {
+    'Accept': 'application/json',
+}
+params = (
+    ('page', '1'),
+    ('pageSize', '100'),
+    ('apiKey', 'ca2f08c5-8b33-454a-adc4-8215cfb3e088'),
+)
+# # PURE get request
+response = requests.get('https://pure01.tugraz.at/ws/api/514/changes/' + str(date.today()), headers=headers, params=params)
+open(dirpath + "/reports/resp_pure_changes.json", 'wb').write(response.content)
+resp_json = json.loads(response.content)
+
+# resp_json = open(dirpath + '/reports/resp_pure.json', 'r')                  # -- TEMPORARY -- 
+# resp_json = json.load(resp_json)                                            # -- TEMPORARY -- 
+
+uuids = ''
+
+for item in resp_json['items']:
+    print(f"{item['changeType']} - {item['uuid']}")
+    if item['changeType'] == 'UPDATE':
+        uuids += item['uuid'] + '\n'
+
+open(dirpath + "/reports/to_re_transfer.log", "a").write(uuids)
+
+# os.system('/usr/bin/python /home/bootcamp/src/pure_sync_rdm/synchronizer/2_reTransmit.py')
