1ff4cbe41c28046369c56f3ea8f90c61851ec639
mat-gro <m.grosso@tugraz.at>
2020-05-28 10:53:00 +0200

    Read rdm record and create simple pure xml import

--- synchronizer/source/pure/import_records.py
+++ synchronizer/source/pure/import_records.py
@@ -10,12 +10,9 @@ class ImportRecords:
 
     def run_import(self):
         
-        rdm_data = self._get_rdm_record_metadata()
+        item = self._get_rdm_record_metadata()
         
-        self._create_xml(rdm_data)
-        # self._create_xml_test()
-
-
+        self._create_xml(item)
 
 
     def _create_xml(self, item):
@@ -32,77 +29,75 @@ class ImportRecords:
                                 id=item['uuid'],
                                 type='dataset')
 
-        ET.SubElement(body, "{%s}title" % ns_dataset).text = item['title']
+        self._add_sub_element(body, ns_dataset, 'title', [], item['title'])
 
-        # ET.SubElement(body, "{%s}managingOrganisation" % ns_dataset, lookupId=item['managingOrganisationalUnit_externalId'])
+        self._add_sub_element(body, ns_dataset, 'managingOrganisation', [['lookupId', item['managingOrganisationalUnit_externalId']]])
 
-        # persons = ET.SubElement(body, "{%s}persons" % ns_dataset, 
-        #                                 id=item['contributors'][0]['uuid'],    # REVIEW!!!!
-        #                                 contactPerson='true')
-        # ET.SubElement(persons, "{%s}person" % ns_dataset, lookupId=item['contributors'][0]['externalId'])
-        # ET.SubElement(persons, "{%s}role" % ns_dataset).text = item['contributors'][0]['personRole']
-        # ET.SubElement(persons, "{%s}associationStartDate" % ns_dataset).text = '2002-02-02'    # REVIEW!!!!
+        persons = self._add_sub_element(body, ns_dataset, 'persons', [['id', item['contributors'][0]['uuid']], ['contactPerson', 'true']])
 
-        # date = ET.SubElement(body, "{%s}availableDate" % ns_dataset)
-        # ET.SubElement(date, "{%s}year" % ns_commons).text = item['publication_date']
+        self._add_sub_element(persons, ns_dataset, 'person', [['lookupId', item['contributors'][0]['externalId']]])
+        self._add_sub_element(persons, ns_dataset, 'role', [], item['contributors'][0]['personRole'])
+        self._add_sub_element(persons, ns_dataset, 'associationStartDate', [], '2002-02-02')    # REVIEW!!!!
 
-        # publisher = ET.SubElement(body, "{%s}publisher" % ns_dataset, 
-        #                                 lookupId="none")    # REVIEW!!!!
-        # ET.SubElement(publisher, "{%s}name" % ns_dataset).text = 'none'
-        # ET.SubElement(publisher, "{%s}type" % ns_dataset).text = 'none'
+        date = self._add_sub_element(body, ns_dataset, 'availableDate')
+        self._add_sub_element(date, ns_commons, 'year', [], item['publication_date'])
+
+        publisher = self._add_sub_element(body, ns_dataset, 'publisher', [['lookupId', "none"]])    # REVIEW!!!!
+        self._add_sub_element(publisher, ns_dataset, 'name', [], 'none')
+        self._add_sub_element(publisher, ns_dataset, 'type', [], 'none')
 
         # Wrap it in an ElementTree instance and save as XML
         xml_str = minidom.parseString(ET.tostring(root)).toprettyxml(indent="   ")
         open(self.file_name, "w").write(xml_str)
 
 
-    def _add_sub_element(self, element: object, namespace: str, sub_element_name: str, attributes = {}, value = ''):
+    def _add_sub_element(self, element: object, namespace: str, sub_element_name: str, attributes = [], value = ''):
 
-        ET.SubElement(element, "{%s}%s" % (namespace, sub_element_name))
+        # value = self._get_value(self.item, path)
+
+        sub_element = ET.SubElement(element, "{%s}%s" % (namespace, sub_element_name))
+
+        for attribute in attributes:
+            if attribute[1]:
+                sub_element.set(attribute[0], attribute[1])
+
+        if value:
+            sub_element.text = value
+
+        return sub_element
 
 
-    def _create_xml_test(self):
-        
-        ns_dataset = 'v1.dataset.pure.atira.dk'
-        ns_commons = 'v3.commons.pure.atira.dk'
+    def _get_value(self, item, path: list):
+        """ Gets field value from a given path """
+        child = item
+        count = 0
+        # Iterates over the given path
+        for i in path:
+            # If the child (step in path) exists or is equal to zero
+            if i in child or i == 0:
+                # Counts if the iteration took place over every path element
+                count += 1
+                child = child[i]
+            else:
+                return False
 
-        ET.register_namespace('v1',ns_dataset)
-        ET.register_namespace('v3',ns_commons)
+        # If the full path is not available (missing field)
+        if len(path) != count:
+            return False
 
-        # Build a tree structure
-        root = ET.Element("{%s}datasets" % ns_dataset)
+        value = str(child)
 
-        body = ET.SubElement(root, "{%s}dataset" % ns_dataset, 
-                                id="50cc689b-95d9-4d2d-b848-25f52eb01a84",
-                                type='dataset')
-
-        ET.SubElement(body, "{%s}title" % ns_dataset).text = 'testtitle'
-        ET.SubElement(body, "{%s}managingOrganisation" % ns_dataset, lookupId='20753')
-
-        persons = ET.SubElement(body, "{%s}persons" % ns_dataset, 
-                                        id='c0a3f804-fb58-4bbb-9ee4-0a3f7276c0c8',
-                                        contactPerson='true')
-        ET.SubElement(persons, "{%s}person" % ns_dataset, lookupId='24990')
-        ET.SubElement(persons, "{%s}role" % ns_dataset).text = 'datacollector'
-        ET.SubElement(persons, "{%s}associationStartDate" % ns_dataset).text = '2002-02-02'
-
-        date = ET.SubElement(body, "{%s}availableDate" % ns_dataset)
-        ET.SubElement(date, "{%s}year" % ns_commons).text = '2013'
-
-        publisher = ET.SubElement(body, "{%s}publisher" % ns_dataset, 
-                                        lookupId="45d22915-6545-4428-896a-8b8046191d5d")
-        ET.SubElement(publisher, "{%s}name" % ns_dataset).text = 'New publisher'
-        ET.SubElement(publisher, "{%s}type" % ns_dataset).text = 'publisher'
-
-        # Wrap it in an ElementTree instance and save as XML
-        xml_str = minidom.parseString(ET.tostring(root)).toprettyxml(indent="   ")
-        open(self.file_name, "w").write(xml_str)
-
+        # REPLACEMENTS
+        value = value.replace('\t', ' ')        # replace \t with ' '
+        value = value.replace('\\', '\\\\')     # adds \ before \
+        value = value.replace('"', '\\"')       # adds \ before "
+        value = value.replace('\n', '')         # removes new lines
+        return value
 
 
     def _get_rdm_record_metadata(self):
+        # response = self.rdm_requests.get_metadata('', '9zs2w-cj227')
         response = self.rdm_requests.get_metadata('', '05qm8-ats84')
-        print(response)
 
         if response.status_code >= 300:
             return False
@@ -110,14 +105,3 @@ class ImportRecords:
         # Load response
         return json.loads(response.content)['metadata']
 
-
-
-
-# root = ET.Element("root")
-# doc = ET.SubElement(root, "doc")
-
-# ET.SubElement(doc, "field1", name="blah").text = "some value1"
-# ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-
-# tree = ET.ElementTree(root)
-# tree.write("/home/bootcamp/src/pure_sync_rdm/synchronizer/tests/test.xml")
