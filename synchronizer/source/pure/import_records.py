3b42df7ca36d31fc1878cbcf686ec54d2830c6f6
mat-gro <m.grosso@tugraz.at>
2020-05-28 12:41:51 +0200

    Pure import - xml creation structure field attribute and text

--- synchronizer/source/pure/import_records.py
+++ synchronizer/source/pure/import_records.py
@@ -9,16 +9,18 @@ class ImportRecords:
         self.file_name = "/home/bootcamp/src/pure_sync_rdm/synchronizer/data/temporary_files/test.xml"
 
     def run_import(self):
-        
-        item = self._get_rdm_record_metadata()
-        
+
+        item = self._get_rdm_record_metadata('9zs2w-cj227')
+        self._create_xml(item)
+
+        item = self._get_rdm_record_metadata('05qm8-ats84')
         self._create_xml(item)
 
 
     def _create_xml(self, item):
 
-        ns_dataset = 'v1.dataset.pure.atira.dk'
-        ns_commons = 'v3.commons.pure.atira.dk'
+        ns_dataset = 'v1.dataset.pure.atira.dk'     # Name Space dataset
+        ns_commons = 'v3.commons.pure.atira.dk'     # Name Space commons
 
         ET.register_namespace('v1',ns_dataset)
         ET.register_namespace('v3',ns_commons)
@@ -28,43 +30,71 @@ class ImportRecords:
         body = ET.SubElement(root, "{%s}dataset" % ns_dataset, 
                                 id=item['uuid'],
                                 type='dataset')
+        # Title
+        title = self._sub_element(body, ns_dataset, 'title')
+        self._add_text(item, title, ['title'])
 
-        self._add_sub_element(body, ns_dataset, 'title', [], item['title'])
+        # Managing organisation
+        organisational_unit = self._sub_element(body, ns_dataset, 'managingOrganisation')
+        attributes = [['lookupId', ['managingOrganisationalUnit_externalId']]]
+        self._add_attribute(item, organisational_unit, attributes)
 
-        self._add_sub_element(body, ns_dataset, 'managingOrganisation', [['lookupId', item['managingOrganisationalUnit_externalId']]])
+        # Persons
+        persons = self._sub_element(body, ns_dataset, 'persons')
+        attributes = [['id', ['contributors', 0, 'uuid']], ['contactPerson', 'true']]
+        self._add_attribute(item, persons, attributes)
 
-        persons = self._add_sub_element(body, ns_dataset, 'persons', [['id', item['contributors'][0]['uuid']], ['contactPerson', 'true']])
+        person = self._sub_element(persons, ns_dataset, 'person')
+        attributes = [['lookupId', ['contributors', 0, 'externalId']]]
+        self._add_attribute(item, person, attributes)
 
-        self._add_sub_element(persons, ns_dataset, 'person', [['lookupId', item['contributors'][0]['externalId']]])
-        self._add_sub_element(persons, ns_dataset, 'role', [], item['contributors'][0]['personRole'])
-        self._add_sub_element(persons, ns_dataset, 'associationStartDate', [], '2002-02-02')    # REVIEW!!!!
+        role = self._sub_element(persons, ns_dataset, 'role')
+        self._add_text(item, role, ['contributors', 0, 'personRole'])
 
-        date = self._add_sub_element(body, ns_dataset, 'availableDate')
-        self._add_sub_element(date, ns_commons, 'year', [], item['publication_date'])
+        start_date = self._sub_element(persons, ns_dataset, 'associationStartDate')    # REVIEW!!!!
+        self._add_text(item, start_date, 'not in rdm - ???')
 
-        publisher = self._add_sub_element(body, ns_dataset, 'publisher', [['lookupId', "none"]])    # REVIEW!!!!
-        self._add_sub_element(publisher, ns_dataset, 'name', [], 'none')
-        self._add_sub_element(publisher, ns_dataset, 'type', [], 'none')
+        # Available date
+        date = self._sub_element(body, ns_dataset, 'availableDate')
+
+        sub_date = self._sub_element(date, ns_commons, 'year')
+        self._add_text(item, sub_date, ['publication_date'])
+
+        # Publisher
+        publisher = self._sub_element(body, ns_dataset, 'publisher')    # REVIEW!!!!
+        self._sub_element(publisher, ns_dataset, 'name')
+        self._sub_element(publisher, ns_dataset, 'type')
 
         # Wrap it in an ElementTree instance and save as XML
         xml_str = minidom.parseString(ET.tostring(root)).toprettyxml(indent="   ")
         open(self.file_name, "w").write(xml_str)
 
 
-    def _add_sub_element(self, element: object, namespace: str, sub_element_name: str, attributes = [], value = ''):
+    def _sub_element(self, element: object, namespace: str, sub_element_name: str):
 
-        # value = self._get_value(self.item, path)
+        return ET.SubElement(element, "{%s}%s" % (namespace, sub_element_name))
 
-        sub_element = ET.SubElement(element, "{%s}%s" % (namespace, sub_element_name))
 
+    def _add_attribute(self, item, sub_element, attributes):
+    
         for attribute in attributes:
-            if attribute[1]:
-                sub_element.set(attribute[0], attribute[1])
+            value = self._check_and_get_value(item, attribute[1])
+            if value:
+                sub_element.set(attribute[0], value)
 
-        if value:
-            sub_element.text = value
 
-        return sub_element
+    def _add_text(self, item, sub_element, path):
+        
+        sub_element.text = self._check_and_get_value(item, path)
+
+
+
+    def _check_and_get_value(self, item, path):
+        
+        if type(path) == str:
+            return path
+        else:
+            return self._get_value(item, path)
 
 
     def _get_value(self, item, path: list):
@@ -95,13 +125,11 @@ class ImportRecords:
         return value
 
 
-    def _get_rdm_record_metadata(self):
-        # response = self.rdm_requests.get_metadata('', '9zs2w-cj227')
-        response = self.rdm_requests.get_metadata('', '05qm8-ats84')
+    def _get_rdm_record_metadata(self, recid):
+        response = self.rdm_requests.get_metadata('', recid)
 
         if response.status_code >= 300:
             return False
 
         # Load response
         return json.loads(response.content)['metadata']
-
